286c2a91afb33f286210c5e22c52885379331d65
viktor <harevicviktor@gmail.com>
2025-09-08 11:58:51 +0300

    feat: add bashrc and aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,13 @@ sudo apt update -y
 sudo apt install -y stow
 echo "[✓] Done. Version: $(stow --version | head -n1)"
 
-# --- Stow i3 and i3status configs ---
+# --- Stow configs ---
 cd "$HOME/dotfiles"
+
+# i3 + i3status
 stow i3
 echo "[✓] Linked i3 + i3status configs"
+
+# aliases
+stow aliases
+echo "[✓] Linked aliases"
